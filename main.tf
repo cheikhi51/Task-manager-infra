d063cdc86de6c0e771b7ce56790e081416cd461f
Mohamed <cheikhimohamed51@gmail.com>
2026-03-30 14:11:55 +0100

    added the kubeconfig path

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = var.kubeconfig_path
+}
+
+variable "kubeconfig_path" {
+  type = string
 }
 
 variable "namespace" {}
 variable "image_tag" {}
 
 # 1. Namespace
-resource "kubernetes_namespace" "ns" {
+resource "kubernetes_namespace_v1" "ns" {
   metadata {
     name = var.namespace
   }
