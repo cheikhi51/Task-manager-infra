3d7251ef5818bdb1b2773078eac1ea725264bba8
Mohamed <cheikhimohamed51@gmail.com>
2026-04-01 10:56:35 +0100

    removed the argocd part

--- main.tf
+++ main.tf
@@ -15,38 +15,3 @@ resource "kubernetes_namespace_v1" "ns" {
     name = var.namespace
   }
 }
-
-# 2. ArgoCD Application
-resource "kubernetes_manifest" "argocd_app" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "Application"
-
-    metadata = {
-      name      = "${var.namespace}"
-      namespace = "argocd"
-    }
-
-    spec = {
-      project = "default"
-
-      source = {
-        repoURL        = "https://github.com/cheikhi51/Task-Manager-K8s.git"
-        targetRevision = "main"
-        path           = "overlays/${var.namespace}"
-      }
-
-      destination = {
-        server    = "https://kubernetes.default.svc"
-        namespace = var.namespace
-      }
-
-      syncPolicy = {
-        automated = {
-          prune    = true
-          selfHeal = true
-        }
-      }
-    }
-  }
-}
