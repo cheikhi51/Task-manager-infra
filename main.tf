2392e9aac9e2225c2807759dc1e1e0a4bad45b49
Mohamed <cheikhimohamed51@gmail.com>
2026-04-03 15:30:11 +0100

    added a local backend to provision each pr state

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  # Backend local avec path dynamique
+  # Le path sera injecté via -backend-config au moment du init
+  backend "local" {}
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "kubernetes" {
   config_path = var.kubeconfig_path
 }
